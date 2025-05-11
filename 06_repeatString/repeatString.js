8afc1e7a2a0c6074adbee29f21c4492ae6a3fb20
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-11 16:57:03 +0200

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let toBeReturned = "";
+    for (let i = 0; i < num; i ++) {
+        toBeReturned += string;
+    }
+    return toBeReturned;
 };
 
 // Do not edit below this line
