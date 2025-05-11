f0092117c5646e2c8895445fd931cb5fc0f543b7
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-11 17:41:07 +0200

    Complete exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...parameters) {
+    let toBeReturned = array;
+    console.log(toBeReturned);
+    console.log(parameters);
+    for (let parameter of parameters) {
+        toBeReturned = toBeReturned.filter((element) => parameter !== element);
+        console.log(`iterated through parameter: ${parameter}, array now looks like this: ${toBeReturned}`);
+    }
+    console.log(toBeReturned);
+    return toBeReturned;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
