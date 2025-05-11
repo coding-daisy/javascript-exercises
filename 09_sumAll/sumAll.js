a7b0904ff9667ee38e1f72aa04ca7be7aec96dd9
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-11 17:52:46 +0200

    COmplete exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+
+    if ((from < 0) || (to < 0) || !Number.isInteger(from) || !Number.isInteger(to)) {
+        return "ERROR";
+    }
+
+    let lower;
+    let higher;
+    if (from <= to) {
+        lower = from;
+        higher = to;
+    } else {
+        lower = to;
+        higher = from;
+    }
+
+    let toBeReturned = 0;
+    for (let i = lower; i <= higher; i++) {
+        toBeReturned += i;
+    }
+    return toBeReturned;
 
 };
 
