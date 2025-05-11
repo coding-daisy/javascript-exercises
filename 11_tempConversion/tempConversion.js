4848ddf58f71b4ca89443ecdd173a60534991fdf
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-11 18:06:48 +0200

    Complete exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureInCelsius = (temperatureInFahrenheit - 32) * (5/9);
+  return (Math.round(temperatureInCelsius*10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let temperatureInFahrenheit = (temperatureInCelsius * (9/5)) + 32;
+  return (Math.round(temperatureInFahrenheit*10)/10);
 };
 
 // Do not edit below this line
