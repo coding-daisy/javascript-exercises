91bbe513a90605c7df410bfd60a3901b43de6408
coding-daisy <simplydaisyy.101@gmail.com>
2025-05-11 17:02:15 +0200

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let toBeReturned = "";
+    for (let i = string.length - 1; i >= 0; i --) {
+        toBeReturned += string[i];
+    }
+    return toBeReturned;
 };
 
 // Do not edit below this line
